d74d8d416ad37d9add43a109324d2a46b4c7ccc7
geovanePNX <geovanejt10@gmail.com>
2026-06-17 10:31:20 -0300

    Modify database connection to include portConectando databse
    Updated connection to use port variable for MySQL.

--- Aula 10 (PHP)/connection.php
+++ Aula 10 (PHP)/connection.php
@@ -4,6 +4,12 @@ $host = "localhost";
 $user = "root";
 $password = "root";
 $database = "aula_sesi";
-$spot = 3307;
+$port = 3307;
+try{
+    $conn = mysqli_connect($host, $user, $password, $port);
+    echo"Conectado com o banco de dados MYSQL.";
+} catch(Exception $e) {
+    echo $e;
+}
 
 ?>
