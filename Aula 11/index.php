913b09f4b68dfeaf8533336ec68e2557fc81c34f
geovane.pnx <geovanejt10@gmail.com>
2026-06-24 09:17:19 -0300

    Aula 11

---
+++ Aula 11/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action = "coneection.php" method="post">
+    <input type="text" name="nome" placeholder="Digite seu nome...">
+    <input type="text" name="email" placeholder="Digite seu email..."> 
+    <input type="text" name="senha" placeholder="Digite sua senha..."> 
+    <input type="submit" value="enviar"> 
+   </form> 
+</body>
+</html>
